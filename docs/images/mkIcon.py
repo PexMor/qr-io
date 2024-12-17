9df47c15137311c1b9c383fec3293d32bf8a7505
PexMor <p.moravek@xlinux.cz>
2024-12-17 19:21:13 +0100

    resize icon

--- docs/images/mkIcon.py
+++ docs/images/mkIcon.py
@@ -1,18 +1,21 @@
-import qrcode
-import json
-from PIL import Image, ImageDraw
+#!/usr/bin/env -S python
 
+import json
+
+import qrcode
+from PIL import Image, ImageDraw
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import CircleModuleDrawer, SquareModuleDrawer
 
 qr = qrcode.QRCode(
-    version=10,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=8,
-    border=4,
+    border=7,
     mask_pattern=4,
 )
-jdata = json.dumps({"hello": "world"})
+jdata: str = json.dumps({"hello": "world"})
+jdata = "QR"
 qr.add_data(jdata)
 qr.make(fit=True)
 
@@ -20,7 +23,7 @@ img = qr.make_image(
     fill_color="white",
     back_color=None,
     image_factory=StyledPilImage,
-    module_drawer=CircleModuleDrawer(resample_method=None),
+    module_drawer=SquareModuleDrawer(resample_method=None),
     eye_drawer=SquareModuleDrawer(),
 )
 
